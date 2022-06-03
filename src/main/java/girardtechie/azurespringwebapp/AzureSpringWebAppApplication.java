0c59fac85f762f64b751b17fd7b72425bb0a88ee
saragirard <sarag.bsa@gmail.com>
2022-06-03 13:15:27 -0400

    first commit

---
+++ src/main/java/girardtechie/azurespringwebapp/AzureSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package girardtechie.azurespringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureSpringWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/girardtechie/azurespringwebapp/AzureSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package girardtechie.azurespringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
