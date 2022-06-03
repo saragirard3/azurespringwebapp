1cb4923972e2766ffd3a1301e11e297c2f059f63
saragirard <sarag.bsa@gmail.com>
2022-06-03 13:33:00 -0400

    update to get site active

--- src/main/java/girardtechie/azurespringwebapp/controller/IndexController.java
+++ src/main/java/girardtechie/azurespringwebapp/controller/IndexController.java
@@ -1,11 +1,13 @@
 package girardtechie.azurespringwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
+    @GetMapping("/")
     public ModelAndView Index(){
         ModelAndView model = new ModelAndView();
         model.setViewName("index");
